1d7e8354482798d07d24d470cda971fa0881ec64
ChungTM1209 <minhchung.ctk17@gmail.com>
2019-04-10 17:42:24 +0700

    create UserTableSeeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new \App\User();
+        $user->name = 'admin';
+        $user->email = 'admin@gmail.com';
+        $user->password = bcrypt('123456');
+        $user->phone = '0123456789';
+        $user->address = '123@abc';
+        $user->role = \App\Http\Controllers\RoleConstant::ADMIN;
+        $user->image = null;
+        $user->save();
+
+    }
+}
